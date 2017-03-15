e4efef38d58f623ea6c5455a04e82fb170024019
AndroidHot <andyhotnewlife@gmail.com>
2017-03-15 10:01:52 +0800

    Adapter runtime permissions.

--- app/src/main/java/com/app/taoxin/view/activity/MeiZhiActivity.java
+++ app/src/main/java/com/app/taoxin/view/activity/MeiZhiActivity.java
@@ -1,10 +1,15 @@
 package com.app.taoxin.view.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -20,7 +25,6 @@ import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
-
 
 public class MeiZhiActivity extends AppCompatActivity {
 
@@ -100,6 +104,7 @@ public class MeiZhiActivity extends AppCompatActivity {
                 ShareUtil.shareImage(this, ImageUtil.saveImage(this, url, bitmap, image, "share"));
                 break;
             case R.id.action_save:
+                checkSelfPermission();
                 ImageUtil.saveImage(this, url, bitmap, image, "save");
                 break;
             case R.id.action_click_me:
@@ -107,5 +112,23 @@ public class MeiZhiActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void checkSelfPermission() {
+        if (ContextCompat.checkSelfPermission(MeiZhiActivity.this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+            PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MeiZhiActivity.this,
+                new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE }, 1);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
